cb78018b98d6ab4b4394c4b0274d40855393c318
Lukas Code <lukas-code@outlook.com>
2018-01-16 22:48:01 +0100

    Fenster anzeigen

--- Vorurteile/src/fakten/Main.java
+++ Vorurteile/src/fakten/Main.java
@@ -1,18 +1,16 @@
-package vorurteile;
+package fakten;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Scene scene = new Scene(FXMLLoader.load(getClass().getResource("EingabeFaktView.fxml")));
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
